c95f569fb736530169c303f66d99b48215f72ead
ihtiandr9 mobile <lomaster@ihtiandr9.fvds.ru>
2026-06-13 13:36:57 +0800

    tests
    - add .gitignore
    - try to override ToInt

--- eqsq/equation.cs
+++ eqsq/equation.cs
@@ -4,39 +4,39 @@ namespace Equations
 {
     class Coefficient
     {
-        int _val;
+        int val;
         public Coefficient(int val)
         {
-            _val = val;
+            this.val = val;
         }
         public override string ToString()
         {
-            if (_val == 1) return "";
-            return _val.ToString();
+            if (val == 1) return "";
+            return val.ToString();
         }
-        public int Value()
+        public override ToIint()
         {
-            return _val;
+            return val;
         }
     }
     class Equation
     {
-        Coefficient _a;
-        Coefficient _b;
-        Coefficient _c;
+        Coefficient a;
+        Coefficient b;
+        Coefficient c;
         public Equation(int a, int b, int c)
         {
-            _a = new Coefficient(a);
-            _b = new Coefficient(b);
-            _c = new Coefficient(c);
+            this.a = new Coefficient(a);
+            this.b = new Coefficient(b);
+            this.c = new Coefficient(c);
         }
 
         public void PrintMe()
         {
             string eqString = "";
-            if (_a.Value() != 0) eqString = _a.ToString() + "x^2";
-            if (_b.Value() != 0) eqString += " + " + _b.ToString() + "x";
-            if (_c.Value() != 0) eqString += " + " + _c.ToString();
+            if (a != 0) eqString = a.ToString() + "x^2";
+            if (b != 0) eqString += " + " + b.ToString() + "x";
+            if (c != 0) eqString += " + " + c.ToString();
             eqString += " = 0";
             Console.WriteLine("Solving Equation\n");
             Console.WriteLine(eqString);            
